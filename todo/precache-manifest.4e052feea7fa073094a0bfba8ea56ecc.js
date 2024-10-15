e90411f5f0ae2680588e5d8aded66dc92ee70e9b
Bagai35 <60263970+Bagai35@users.noreply.github.com>
2024-10-15 10:37:20 +0300

    123

---
+++ todo/precache-manifest.4e052feea7fa073094a0bfba8ea56ecc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d33d26403b6ea52b37be",
+    "url": "%3Ckichatik%3E.github.io/todo/static/css/main.c117e5e5.chunk.css"
+  },
+  {
+    "revision": "d33d26403b6ea52b37be",
+    "url": "%3Ckichatik%3E.github.io/todo/static/js/main.d33d2640.chunk.js"
+  },
+  {
+    "revision": "46ef72d454fff641b9e6",
+    "url": "%3Ckichatik%3E.github.io/todo/static/css/1.7e3201ae.chunk.css"
+  },
+  {
+    "revision": "46ef72d454fff641b9e6",
+    "url": "%3Ckichatik%3E.github.io/todo/static/js/1.46ef72d4.chunk.js"
+  },
+  {
+    "revision": "4e42260e6865add6169f",
+    "url": "%3Ckichatik%3E.github.io/todo/static/js/runtime~main.4e42260e.js"
+  },
+  {
+    "revision": "c623726e9d792c5094da5ef5af39bc7b",
+    "url": "%3Ckichatik%3E.github.io/todo/index.html"
+  }
+];
