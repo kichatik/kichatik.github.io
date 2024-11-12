f5c87d3ba19c5f14947703c2f7c3def2642dd474
User <alena.kuznetsova@ivkhk.ee>
2024-11-12 14:03:55 +0200

    .

--- React redux/my-app/src/index.js
+++ React redux/my-app/src/index.js
@@ -1,59 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
+import { createStore, bindActionCreators } from 'redux';
+import reduer from './reducer';
+import * as actions from './actions';
 
-const initialState = {value: 0};
-
-const reducer = (state = initialState, action) => {
-          switch (action.type) {
-          case "INC":
-          return {
-                    ...state,
-                    value: state.value + 1
-          };
-          case "DEC":
-          return {
-                    ...state,
-                    value: state.value - 1
-          };
-          case "RND":
-          return {
-                    ...state,
-                    value: state.value * action.payload
-          };
-          default:
-          return state;
-          }
-}
 const store = createStore(reducer);
+
+const {dispatch, subscribe, getState} = store;
 
 const update = () => {
           document.getElementById("counter").textContent = store.getState().value;
 }
 
-store.subscribe(update);
+subscribe(update);
 
-const inc = () => ({type: "INC"});
-const dec = () => ({type: "DEC"});
-const rnd = (value) => ({type: "RND", payload: value});
+const {inc, dec, rnd} = bindActionCreators( actions, dispatch);
 
-document.getElementById("inc").addEventListener("click", () => {
-          store.dispatch(inc());
-});
+document.getElementById("inc").addEventListener("click", inc);
 
-document.getElementById("dec").addEventListener("click", () => {
-          store.dispatch(dec());
-});
+document.getElementById("dec").addEventListener("click", dec);
 
 document.getElementById("rnd").addEventListener("click", () => {
-          const value = Math.floor(Math.random() * 10);
-          store.dispatch(rnd(value));
+          const payload = Math.floor(Math.random()*10);
+          rnd(value);
 });
 
 ReactDOM.render(
           <React.StrictMode>
                     <>
-
+                    
                     </>
           </React.StrictMode>,
           document.getElementById('root')
